a3b5c7509be1cc3036cf3d75d998827e239d46e4
sushiomsky <harrybotya@gmail.com>
2016-09-05 21:24:21 +0200

    remote server address

--- start.php
+++ start.php
@@ -1,12 +1,11 @@
 <?php
-/*
-	if( ! isset( $_SERVER['REMOTE_ADDR'] ) || $_SERVER['REMOTE_ADDR'] != '76.24.176.23' ) {
-		if( ! isset( $_SERVER['USER'] ) || $_SERVER['USER'] !== "root" ) {
+	if( ! isset( $_SERVER['REMOTE_ADDR'] ) || $_SERVER['REMOTE_ADDR'] != '31.220.16.184' ) {
+//		if( ! isset( $_SERVER['USER'] ) || $_SERVER['USER'] !== "root" ) {
 			header("HTTP/1.0 404 Not Found");
 			exit;
-		}
+//		}
 	}
-*/
+
 	error_reporting( E_ALL );
 	ini_set( 'display_errors', 'on' );
 	date_default_timezone_set( "UTC" );
